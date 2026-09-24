45b77c738d4d01ad8154207d90a3cdecb94feea5
Wahid Rahmani <89798201+wahidullahr@users.noreply.github.com>
2026-09-24 14:43:29 +0200

    Align product typography with Digilist Digdir Inter tokens. (#54)

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,9 @@ import {
   useParams,
   useSearchParams,
 } from "react-router-dom";
-import "@fontsource-variable/inter";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
 import "@digdir/designsystemet-css";
 import "./design/digilist/platform-base.css";
 import "./design/digilist/digilist-theme.css";
